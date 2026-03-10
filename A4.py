9f86ae8f27db1d9c72785cd820226c5d83c1ba8c
ksenera <ksenera7130@conestogac.on.ca>
2026-03-10 14:55:06 -0400

    header comment

---
+++ A4.py
@@ -0,0 +1,8 @@
+"""
+PROJECT   : A4.py
+PROGRAMMER: Kushika Senera
+COURSE    : SFWRTECH 4MA3 - Numerical Linear Algebra and Numerical Optimization
+INSTRUCTOR: Gagan Sidhu
+DATE: Tuesday, March 10th 
+"""
+
